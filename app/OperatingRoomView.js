451aaeb2b23db663bc55f28798cea1b27a2aeb0a
adorsk <adorsk@gmail.com>
2015-05-22 15:55:04 -0400

    start of rolling gurneys

--- app/OperatingRoomView.js
+++ app/OperatingRoomView.js
@@ -6,20 +6,26 @@
 var Backbone = require('backbone');
 var _ = require('underscore');
 var Marionette = require('./marionette-shim');
+require('jquery-ui');
 
 var CharacterView = require('./CharacterView');
 
 var OperatingRoomView = Marionette.LayoutView.extend({
     className: 'hh-operating-room',
     template: _.template(''
-        + '<div class="gurney"><div class="gurney-top"><div class="label">Patient</div></div><div class="gurney-bottom"><div class="patient-region gurney-content"></div></div></div>'
-        + '<div class="gurney"><div class="gurney-top"><div class="label">Donor</div></div><div class="gurney-bottom"><div class="donor-region gurney-content"></div></div></div>'
+        + '<div class="gurney patient"><div class="gurney-top"><div class="label">Patient</div></div><div class="gurney-bottom"><div class="patient-region gurney-content"></div></div></div>'
+        + '<div class="gurney donor"><div class="gurney-top"><div class="label">Donor</div></div><div class="gurney-bottom"><div class="donor-region gurney-content"></div></div></div>'
         + ''
     ),
 
     regions: {
         patient: '.patient-region',
         donor: '.donor-region'
+    },
+
+    ui: {
+        patientGurney: '.patient .gurney-bottom',
+        donorGurney: '.donor .gurney-bottom',
     },
 
     modelEvents: {
@@ -40,7 +46,32 @@ var OperatingRoomView = Marionette.LayoutView.extend({
                 )
             });
             this.getRegion('donor').show(new BlankView());
+            this.toggleGurney('donor', {slideIn: {direction: 'right'}});
         }
+    },
+
+    toggleGurney: function(gurneyId, opts) {
+        var $gurney = this.ui[gurneyId + 'Gurney'];
+        var promises = [];
+
+        var effectDefaults = {duration: 'slow'};
+
+        if (opts.fadeIn) {
+            $gurney.css('opacity', .5);
+            promises.push($gurney.fadeTo('short', 1).promise());
+        }
+
+        if (opts.slideIn) {
+            $gurney.css('display', 'none');
+            promises.push($gurney.show('slide', _.extend(effectDefaults, opts.slideIn)));
+        }
+
+        if (opts.slideOut) {
+            $gurney.css('display', 'none');
+            promises.push($gurney.hide('slide', _.extend(effectDefaults, opts.slideOut)));
+        }
+
+        return $.when.apply($, promises);
     },
 
     renderPatient: function(patient) {
@@ -49,17 +80,14 @@ var OperatingRoomView = Marionette.LayoutView.extend({
         var patientView = new CharacterView({model: patientModel});
         this.getRegion('patient').show(patientView);
         patientView.on('drop', this.onDrop, this);
+        return this.toggleGurney('patient', {slideIn: {direction: 'left'}});
     },
 
     renderDonor: function(donor, opts) {
-        opts = _.extend({fadeIn: true}, opts);
         var donorModel = new Backbone.Model(donor);
         var donorView = new CharacterView({model: donorModel});
-        if (opts.fadeIn) {
-            donorView.$el.css('opacity', .5);
-        }
         this.getRegion('donor').show(donorView);
-        donorView.$el.fadeTo('short', 1);
+        return this.toggleGurney('donor', {slideIn: {direction: 'right'}});
     },
 
     onDonorChange: function() {
@@ -68,7 +96,7 @@ var OperatingRoomView = Marionette.LayoutView.extend({
 
     onDrop: function(dropData) {
         this.trigger('evaluated', dropData);
-    }
+    },
 
 });
 
